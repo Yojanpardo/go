e6b1183b1f182c89111e64959b97d72e34e96c28
Yojan Antonio Pardo Zabala <yojan.pardo@gmail.com>
2018-08-20 12:32:31 -0500

    agrego ejercicios de arrays y slices

--- main.go
+++ main.go
@@ -11,6 +11,8 @@ func main(){
   age, email,phone_number := get_personal_data()
   fmt.Printf(HELLO_USER, name, lastname)
   fmt.Printf("tu edad: %d.\ntu email: %s.\ntu numero de telefono: %s.\n",age,email,phone_number)
+  get_hobbies()
+  get_devices()
 }
 
 func get_text() string{
@@ -24,11 +26,28 @@ func get_personal_data() (int,string,string){
   var email string
   var phone_number string
   fmt.Println("Que edad tienes?")
-  fmt.Scanf("%d",age)
+  fmt.Scanf("%d",&age)
   fmt.Println("cual es tu email?")
-  fmt.Scanf("%s",email)
+  fmt.Scanf("%s",&email)
   fmt.Println("Cual es tu numero de telefono?")
-  fmt.Scanf("%s",phone_number)
-
+  fmt.Scanf("%s",&phone_number)
   return age,email,phone_number
+}
+
+func get_hobbies(){
+  var arr1 [2]string
+  fmt.Println("ingresa un hobbie")
+  fmt.Scanf("%s",&arr1[0])
+  fmt.Println("ingresa otro hobbie")
+  fmt.Scanf("%s",&arr1[1])
+  fmt.Printf("tus hobbies son %s y %s.\n",arr1[0],arr1[1])
+}
+
+func get_devices(){
+  var devices []string
+  fmt.Println(devices)
+  devices = append(devices,"celular")
+  fmt.Println(devices)
+  devices = append(devices,"computador")
+  fmt.Println(devices)
 }
