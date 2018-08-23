ae112cc797008edc3eeaee3f6bc391e6109416bc
Yojan Antonio Pardo Zabala <yojan.pardo@gmail.com>
2018-08-23 08:34:14 -0500

    pongo un ciclo infinito no mas para ver como funciona y como funciona el break

--- for_cycle.go
+++ for_cycle.go
@@ -5,25 +5,27 @@ func main(){
   var names []string
   var name string
   friends := 0
-  for friends == 0{ // esto es un for como un while
-    fmt.Println("vamos a hacer una lista de amigos.\nCuantos amigos tienes?")
-    fmt.Scanf("%d", &friends)
-    if friends != 0{
-      for i := 0; i<friends; i++{ //esto es un for normalito
-        if i == 0{
-          fmt.Println("Ingresa un nombre")
-          fmt.Scanf("%s",&name)
-          names = append(names,name)
-        }else if i > 0 {
-          fmt.Println("Ingresa otro nombre")
-          fmt.Scanf("%s",&name)
-          names = append(names,name)
+  for { //así es un ciclo infinito, como un while True
+    for friends == 0{ // esto es un for como un while
+      fmt.Println("vamos a hacer una lista de amigos.\nCuantos amigos tienes?")
+      fmt.Scanf("%d", &friends)
+      if friends != 0{
+        for i := 0; i<friends; i++{ //esto es un for normalito
+          if i == 0{
+            fmt.Println("Ingresa un nombre")
+            fmt.Scanf("%s",&name)
+            names = append(names,name)
+          }else if i > 0 {
+            fmt.Println("Ingresa otro nombre")
+            fmt.Scanf("%s",&name)
+            names = append(names,name)
+          }
         }
+      }else{
+        fmt.Println("ah cabrón! has de tener al menos un amigo, no?")
       }
-      break
-    }else{
-      fmt.Println("ah cabrón! has de tener al menos un amigo, no?")
     }
+    fmt.Println("tus amigos son: ", names)
+    break
   }
-  fmt.Println("tus amigos son: ", names)
 }
